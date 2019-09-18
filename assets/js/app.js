58d256c07dedf23e7f0252f24a6d56e042d926b8
Xavier Davis <xdavis1@icloud.com>
2019-09-18 00:25:07 -0700

    please work

---
+++ assets/js/app.js
@@ -0,0 +1,172 @@
+/* Application for project 1 named "What-Do-You-Want" */
+
+/* ---------------------------------------------------------------------------*/
+
+/* Global variables */
+
+//Grab the users Date
+
+var userDate = $("#userDate").val();
+console.log (userDate);
+
+// grabs the users city
+
+var userCity = $("#userCity").val();
+console.log(userCity);
+
+// grabs the users state
+
+var userState = $("#userState").val();
+console.log(userState);
+
+// object of picked time 
+let timeSlot = {
+    breakfast: {
+      start: "8:00",
+      end: "12:00"
+    },
+    brunch: {
+      start: "10:00",
+      end: "14:00"
+    },
+    lunch: {
+      start: "12:00",
+      end: "16:00"
+    },
+    dinner: {
+      start: "16:00",
+      end: "20:00"
+    },
+    lateNight: {
+      start: "20:00",
+      end: "8:00"
+    }
+  };
+
+/* API Request Section for: Evenful-API, OpenWeather-API, Yelp-API */
+
+/* ---------------------------------------------------------------------------*/
+
+/*  Evenful-API */
+
+// AJAX method
+
+//variable to call concerts
+
+//variable to call movies
+
+//variable to call bars
+
+/* ---------------------------------------------------------------------------*/
+
+/* OpenWeather-API*/
+
+// AJAX method
+
+//variable to call weather provided by the user location
+
+/* ---------------------------------------------------------------------------*/
+
+/* Yelp-API */
+
+// AJAX method
+
+// grab closest restaurants restaurants
+
+// reveiws of said restaurants
+
+/* ---------------------------------------------------------------------------*/
+
+
+ // Get the <span> element that closes the modal
+   
+// Have a current time variable and check the users date to make sure it valid
+
+var currentDate = moment();
+  console.log("CURRENT TIME: " + moment(currentTime).calender());
+     
+
+// creating a status variable to see if meets the requirements 
+var status = false;
+
+/* Dynamic-Buttons */
+let pickedTime = timeSlot[$("#time-Input").val()];
+console.log(pickedTime);
+
+// Submit Button
+const submitBtn = function submit() {
+
+    // this is the vairable for the modal
+let showModal = ("#showModal")
+
+    // taking in all the values from the form
+    userCity.val().trim();
+
+    userState.val();
+
+    userDate.val();
+
+    pickedTime.val();
+
+    // check to make sure all requirements are met so they can go to the next page
+   if( (userDate == "" ) || (userState == "") || (pickedTime == "") || (userCity == "")){
+        (userCity).innerHTML=  "Please enter the city";
+
+        (userState).innerHTML=  "Please enter the state";
+
+        (userDate).innerHTML=  "Please enter the date";
+
+        (pickedTime).innerHTML=  "Please enter a time";
+
+        status = false;
+   } else{
+       status = true;
+   }
+
+// first it will check the first line for any word OR it will check and see if the city has two words instead of just one.
+if ( (userCity == "^[A-Za-z]|^[A-Za-z]\s[A-Za-z]") ){
+
+  (userCity).innerHTML = "Please enter letters";
+  status = false;
+} else{
+  status = true;
+}
+
+
+
+/* Card outputs information*/
+
+// Card-Details
+
+// Card-Event-1
+
+// Card-Event-2
+
+// Card-Event-3
+
+};
+
+
+$(".btn").on("submit", submitBtn);
+
+// Randomize Button
+const tryAgainBtn = function randomize() {};
+
+
+$(".btn").on("tryAgain", tryAgainBtn);
+// Clear Button
+const clearBtn = function clear() {
+  $("#userTime").val("");
+  $("#Destination-input").val("");
+};
+
+/* ---------------------------------------------------------------------------*/
+
+/* Event listener function */
+
+
+/* Second page */
+
+
+
+<script src="VerbalExpressions.js"></script>
