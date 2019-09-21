2adbee46eb46adb7b1e0ca1231a67bdb133b4fa5
SirHexxus <jamesmichaelstacy@gmail.com>
2019-09-21 09:43:47 -0700

    made code prettier

--- assets/js/apiCalls.js
+++ assets/js/apiCalls.js
@@ -5,36 +5,55 @@
 //  For the Foursquare API
 //  function to call the Foursquare API for the "Meal" result
 let mealCall = function(city, state, time) {
-    let endpoint = "https://api.foursquare.com/v2/venues/search?";
-    let clientInfo = "client_id=3N2N3WZK5ZGFFORDLOQ2PISXVJG1UI5VPYZFFPJY2BD1U1G1&client_secret=QXZWHCJJLB21N2WI1YFUWELH1SULPYKKRKKQSEOCAUXA2ZON";
-    //  Use args to generate queryURL, and make API call
-    let mealQuery = endpoint + clientInfo + "&v=20180323&categoryId=4d4b7105d754a06374d81259&near=" + city + "," + state + "&time=" + time;
-    return mealQuery;
-}
+  let endpoint = "https://api.foursquare.com/v2/venues/search?";
+  let clientInfo =
+    "client_id=3N2N3WZK5ZGFFORDLOQ2PISXVJG1UI5VPYZFFPJY2BD1U1G1&client_secret=QXZWHCJJLB21N2WI1YFUWELH1SULPYKKRKKQSEOCAUXA2ZON";
+  //  Use args to generate queryURL, and make API call
+  let mealQuery =
+    endpoint +
+    clientInfo +
+    "&v=20180323&categoryId=4d4b7105d754a06374d81259&near=" +
+    city +
+    "," +
+    state +
+    "&time=" +
+    time;
+  return mealQuery;
+};
 
 //  function to call the Foursquare API for the "Event" result
 let eventCall = function(city, state, time, timeSlot) {
-    let endpoint = "https://api.foursquare.com/v2/venues/search?";
-    let clientInfo = "client_id=3N2N3WZK5ZGFFORDLOQ2PISXVJG1UI5VPYZFFPJY2BD1U1G1&client_secret=QXZWHCJJLB21N2WI1YFUWELH1SULPYKKRKKQSEOCAUXA2ZON";
-    //  Use args to generate queryURL, and make API call
-    let eventQuery = endpoint + clientInfo + "&v=20180323&near=" + city + "," + state + "&time=" + time;
-    switch (timeSlot) {
-        case "breakfast":
-        case "brunch":
-            eventQuery += "&categoryId=4d4b7105d754a06374d81259";
-            break;
-        case "lunch":
-            eventQuery += "&categoryId=4d4b7105d754a06377d81259";
-            break;
-        case "dinner":
-            eventQuery += "&categoryId=4d4b7105d754a06377d81259,4d4b7104d754a06370d81259";
-            break;
-        case "late night":
-            eventQuery += "&categoryId=4d4b7105d754a06376d81259";
-            break;
-    }
-     return eventQuery;
-}
+  let endpoint = "https://api.foursquare.com/v2/venues/search?";
+  let clientInfo =
+    "client_id=3N2N3WZK5ZGFFORDLOQ2PISXVJG1UI5VPYZFFPJY2BD1U1G1&client_secret=QXZWHCJJLB21N2WI1YFUWELH1SULPYKKRKKQSEOCAUXA2ZON";
+  //  Use args to generate queryURL, and make API call
+  let eventQuery =
+    endpoint +
+    clientInfo +
+    "&v=20180323&near=" +
+    city +
+    "," +
+    state +
+    "&time=" +
+    time;
+  switch (timeSlot) {
+    case "breakfast":
+    case "brunch":
+      eventQuery += "&categoryId=4d4b7105d754a06374d81259";
+      break;
+    case "lunch":
+      eventQuery += "&categoryId=4d4b7105d754a06377d81259";
+      break;
+    case "dinner":
+      eventQuery +=
+        "&categoryId=4d4b7105d754a06377d81259,4d4b7104d754a06370d81259";
+      break;
+    case "late night":
+      eventQuery += "&categoryId=4d4b7105d754a06376d81259";
+      break;
+  }
+  return eventQuery;
+};
 //  This is a space for notes on the Foursquare API, and how we're utilizing it in this app.
 /*
     Client ID: 3N2N3WZK5ZGFFORDLOQ2PISXVJG1UI5VPYZFFPJY2BD1U1G1
@@ -53,12 +72,12 @@ let long;
 
 //  function to call the Darksky API
 let weatherCall = function(lat, long, time) {
-    let endpoint = "https://api.darksky.net/forecast/";
-    let key = "b85c22ea2293cfe948c6f0e6b31705af";
-    //  Use args to generate queryURL, and make API call
-    let weatherQuery = endpoint + key +"/" + lat + "," + long + "," + time;
-    return weatherQuery;
-}
+  let endpoint = "https://api.darksky.net/forecast/";
+  let key = "b85c22ea2293cfe948c6f0e6b31705af";
+  //  Use args to generate queryURL, and make API call
+  let weatherQuery = endpoint + key + "/" + lat + "," + long + "," + time;
+  return weatherQuery;
+};
 //  This is a space for notes on the Open-Weather API, and how we're utilizing it in this app
 /*
     Secret Key: b85c22ea2293cfe948c6f0e6b31705af
--- assets/js/app.js
+++ assets/js/app.js
@@ -47,12 +47,11 @@ const timeSlot = {
 let showModal = function(invalidEntries) {
   $("#badDataModal").modal(); // this line opens modal; next line puts in text
   document.getElementById("badDataModalBody").innerHTML = invalidEntries;
-}
+};
 
 let submitMod = $("#submitMod");
 
 // Get the <span> element that closes the modal
-
 
 // creating a status variable to see if meets the requirements
 let status = false;
@@ -76,36 +75,37 @@ const submitBtn = function submit() {
 
   userDate = $("#date").val();
 
- console.log("this is the Date" + "  " + userDate);
+  console.log("this is the Date" + "  " + userDate);
 
-/*--------- Moved users date inside the submit function ---------*/
+  /*--------- Moved users date inside the submit function ---------*/
 
-//XD3V moved users date input into the submit function,
-userDate = $("#date").val();
+  //XD3V moved users date input into the submit function,
+  userDate = $("#date").val();
 
-// XD3V created new variable named convertedDate that converts the moment function into a variable
-var convertedDate = moment(userDate);
+  // XD3V created new variable named convertedDate that converts the moment function into a variable
+  var convertedDate = moment(userDate);
 
-// XD3V assigned unixTime to equal var convertableDate which will format it in unix time. 
+  // XD3V assigned unixTime to equal var convertableDate which will format it in unix time.
 
-let unixTime = convertedDate.format("X");
-console.log( "this is the unix time  " + unixTime);
+  let unixTime = convertedDate.format("X");
+  console.log("this is the unix time  " + unixTime);
 
-/* ---------------- XD3V placed the new time picked into the submit button function ----------------*/
+  /* ---------------- XD3V placed the new time picked into the submit button function ----------------*/
 
-/* The variable pickedTime is assigned to a Jquery format and it shows what ever the user picks from the drop down menu.
+  /* The variable pickedTime is assigned to a Jquery format and it shows what ever the user picks from the drop down menu.
  Then it once the slection is chose it is linked to the object made named timeSlots pulling the time slots the API needs. */
 
-let pickedTime =  $("#time-Input, option:selected").attr(timeSlot).val();
+  let pickedTime = $("#time-Input, option:selected")
+    .attr(timeSlot)
+    .val();
 
   // XD3V changed pickedTime.val(); into Jquery
   $("pickedTime").val();
   console.log(pickedTime);
-    // check to make sure all requirements are met so they can go to the next page
-    status = true
-    badInput = ""  //  *********** added by fdh to create parameter for function call  ************
-    if ( userDate == "" ) {
-
+  // check to make sure all requirements are met so they can go to the next page
+  status = true;
+  badInput = ""; //  *********** added by fdh to create parameter for function call  ************
+  if (userDate == "") {
     // Please Enter Date
     status = false;
     badInput += "Date<br>"; //  *********** added by fdh to create parameter for function call  ************
@@ -174,7 +174,12 @@ let pickedTime =  $("#time-Input, option:selected").attr(timeSlot).val();
       /*  Foursquare API */
 
       let mealURL = mealCall(userCity, userState, userTime);
-      let eventURL = eventCall(userCity, userState, userTime, $("#time-Input").val());
+      let eventURL = eventCall(
+        userCity,
+        userState,
+        userTime,
+        $("#time-Input").val()
+      );
       let weatherURL = weatherCall(lat, long, unixTime, $("#time-Input").val());
 
       $.ajax({
